74816aba675ad6e13fb3159a221d41cd7a22d085
QRWells <qirui.wang@moegi.waseda.jp>
2023-03-09 05:14:57 +0900

    fix: wrong word split

--- example/WordCount/WordCount.cs
+++ example/WordCount/WordCount.cs
@@ -6,8 +6,7 @@ public class WordCount : IMapper, IReducer
 {
     public IEnumerable<KeyValuePair<string, string>> Map(string key, string value)
     {
-        return value
-            .Split(new[] { ' ' }, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+        return ExtractWords(value)
             .GroupBy(word => word)
             .Select(group => new KeyValuePair<string, string>(group.Key, group.Count().ToString()));
     }
@@ -16,5 +15,22 @@ public class WordCount : IMapper, IReducer
     {
         var sum = values.Sum(int.Parse);
         return sum.ToString();
+    }
+
+    private static IEnumerable<string> ExtractWords(string value)
+    {
+        var list = new List<string>();
+        var span = value.AsSpan();
+        for (var i = 0; i < span.Length; i++)
+        {
+            var c = span[i];
+            if (!char.IsLetter(c)) continue;
+            var start = i;
+            while (i < span.Length && char.IsLetter(span[i]))
+                i++;
+            list.Add(span[start..i].ToString());
+        }
+
+        return list;
     }
 }
